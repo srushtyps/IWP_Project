1c4e251cd1d882daa36bfe034479a486dd889b6d
annkiaa05 <annika.chandra2000@gmail.com>
2022-04-07 22:40:21 +0530

    updated

--- iwp/script.js
+++ iwp/script.js
@@ -1,37 +1,29 @@
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBRAQ5m5ixVOFGRpHyOGovApBUYk7tPowk",
-    authDomain: "iwpproject-b4c68.firebaseapp.com",
-    projectId: "iwpproject-b4c68",
-	databaseURL: "https://iwpproject-b4c68-default-rtdb.asia-northeast3.firebasedatabase.app/",
-    storageBucket: "iwpproject-b4c68.appspot.com",
-    messagingSenderId: "646628849069",
-    appId: "1:646628849069:web:807b400f9a6eed7f0fdd24"
-  };
+const firebaseConfig = {
+	apiKey: "AIzaSyBRAQ5m5ixVOFGRpHyOGovApBUYk7tPowk",
+	authDomain: "iwpproject-b4c68.firebaseapp.com",
+	projectId: "iwpproject-b4c68",
+	storageBucket: "iwpproject-b4c68.appspot.com",
+	messagingSenderId: "646628849069",
+	appId: "1:646628849069:web:807b400f9a6eed7f0fdd24"
+};
 const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
-const form = document.querySelector('.sign-up-container form');
-const inputs = document.querySelectorAll('.sign-up-container input');
+const formSignUp = document.querySelector('.sign-up-container form');
+const inputsSignUp = document.querySelectorAll('.sign-up-container input');
+const formSignIn = document.querySelector('.sign-in-container form');
+const inputsSignIn = document.querySelectorAll('.sign-in-container input');
 
-form.addEventListener('signup', (e) => {
+formSignUp.addEventListener('submit', (e) => {
 	e.preventDefault();
-	// inputs.forEach((input) => {
-	// 	if (!input.value) {
-	// 		input.parentElement.classList.add('error');
-	// 	} else {
-	// 		input.parentElement.classList.remove('error');
-	// 		if (input.type == 'email') {
-	// 			if (validateEmail(input.value)) {
-	// 				input.parentElement.classList.remove('error');
-	// 			} else {
-	// 				input.parentElement.classList.add('error');
-	// 			}
-	// 		}
-	// 	}
-	// });
+	console.log("input received");
 	signUp();
+});
+
+formSignIn.addEventListener('submit', (e) => {
+	e.preventDefault();
+	console.log("input received");
+	signIn();
 });
 
 function validateEmail (email) {
@@ -61,25 +53,22 @@ function changeWindow(){
 
 function writeUserData(name, email) {
 	db.collection("Users").doc(email).set({
-		// first_name: first_name,
-		// last_name: last_name,
 		name : name,
 		email: email
 	}).then(() => {
 		console.log("data written");
 		setTimeout(function(){
-			popup.classList.toggle("popupShow");
+			//popup.classList.toggle("popupShow");
+			window.location.href="./main.html";
 		}, 800);
 	})
 }
 
 function signUp(){
 	console.log("signUp called");
-	_name= document.getElementById('name').value;
-	//last_name= document.getElementById('last-name').value;
-	email= document.getElementById('email').value;
-	password= document.getElementById('password').value;
-	popup = document.getElementById('popup');
+	_name= document.getElementById('nameSU').value;
+	email= document.getElementById('emailSU').value;
+	password= document.getElementById('passwordSU').value;
 	
 	console.log(_name);
 	//console.log(last_name);
@@ -97,7 +86,7 @@ function signUp(){
 		alert("Name cannot be empty");
 		return;
 	}
-	
+	console.log("auth reached");
 	firebase.auth().createUserWithEmailAndPassword(email, password)
 	.then((userCredential) => { 
 		const user = userCredential.user;
@@ -106,8 +95,40 @@ function signUp(){
 	})
 	.catch((error) => {
 		const errorCode = error.code;
-    const errorMessage = error.message;
-	console.log(errorCode);
+    	const errorMessage = error.message;
+		console.log(errorCode);
+		alert(errorMessage);
+  });
+}
+function signIn(){
+	console.log("signIn called");
+	email= document.getElementById('emailSI').value;
+	password= document.getElementById('passwordSI').value;
+
+	console.log(email);
+	console.log(password);
+	
+	
+	if(validateEmail(email) == false || validatePassword(password) ==false){
+		alert("Email or Password in wrong format");
+		
+		return;
+	}
+
+	console.log("auth reached");
+	firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    //var user = userCredential.user;
+	console.log("Log In success");
+	//getUserData(email);
+	window.location.href="./main.html";
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
 	alert(errorMessage);
+	console.log(errorCode);
   });
 }
